71b41731dee7823791a0c7f6ec1aec664bcabd59
Joshua Stresing <pyrotorch69@yahoo.com>
2022-02-14 16:55:37 -0800

    i hate test

--- test/util.test.js
+++ test/util.test.js
@@ -1,7 +1,7 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
-import { renderIngredientLI } from '../util.js';
+import { renderIngredientLI, renderMealLI } from '../util.js';
 
 const test = QUnit.test;
 
@@ -13,6 +13,20 @@ test('time to test a function', (expect) => {
     //Act 
     // Call the function you're testing and set the result to a const
     const actual = renderIngredientLI({ qty: 1, measurement: 'Cups', ingredient: 'sugar' });
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual.outerHTML, expected);
+});
+
+test('time to test a function2', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = `<li>zdfnzdn - 1 Ingredients</li>`;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = renderMealLI({ name: 'zdfnzdn', count: 1 });
 
     //Expect
     // Make assertions about what is expected versus the actual result
